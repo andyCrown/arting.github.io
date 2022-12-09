f3c6219316c84c91f5c26d6df9574e5105a67ba4
andyCrown <389065226@qq.com>
2022-12-09 20:18:54 +0800

    first add
    first add

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+<!--  <div id = 'vue_det'>
+    <h1>site:{{site}}</h1>
+
+  </div> -->
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: '世界和平！',
+      site:'123456'
+    }
+  }
+}
+</script>
